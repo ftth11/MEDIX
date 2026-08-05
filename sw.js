2916c610df57d14929de0860c68ca35371fb435f
ftth11 <136133364+ftth11@users.noreply.github.com>
2026-08-05 09:51:39 +0700

    Add install and fetch event listeners to service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker minimal untuk mendukung instalasi PWA
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('medix-cache-v1').then((cache) => {
+            return cache.addAll(['./index.html', './logo.png']);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
